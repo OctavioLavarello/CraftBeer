980f61e89ce644c59e91586efc771d1c5c965e60
MGConil <germanconil@hotmail.com>
2023-08-02 18:53:18 -0300

    Avances en shop

---
+++ client/src/components/ButtonPages/ButtonPages.tsx
@@ -0,0 +1,21 @@
+import "./ButtonPages.css"
+
+
+const ButtonPages = () => {
+    return (
+        <div className="nav">
+       
+            <button
+                onClick={()=>{}}
+                value="bac"
+            >⇦</button>
+            <h3>{`PAGE ${"numberPage"}`}</h3>
+            <button
+                onClick={()=>{}}
+                value="adv"
+            >⇨</button>
+           
+        </div>
+    )
+}
+export default ButtonPages; 
