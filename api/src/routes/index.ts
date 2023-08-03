59df2de10d4368db82db8ad31aacff55ebfb7f62
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-08-03 13:47:44 -0300

    Borrar getCompanys.ty

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,7 +1,6 @@
 import {Router} from 'express'
 import postUserPerson from "../controller/postUserPerson"
 import postCompany from "../controller/postCompany";
-import getCompanys from '../controller/getCompanys';
 const router = Router();
 
 //-------- post routes -----//
@@ -9,7 +8,7 @@ router.post("/user", postUserPerson);
 router.post("/company", postCompany);
 
 // ------- get routes ------- //
-router.get("/companys", getCompanys);
+
 
 
 // ------- update routes-------//
