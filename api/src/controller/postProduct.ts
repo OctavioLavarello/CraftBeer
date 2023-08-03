d062bffb99c04de05c70e53a83d9be30ef40f13c
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-08-03 19:42:59 -0300

    Elimino qualification de postProduct

--- api/src/controller/postProduct.ts
+++ api/src/controller/postProduct.ts
@@ -9,26 +9,20 @@ const postProduct = async (req: Request, res: Response) => {
       type,
       degreeOfAlcohol,
       description,
-      qualification,
       price,
       stock,
       IBU,
       presentation,
     } = req.body;
 
-    if (!name) res.status(400).json({ message: "name is required" });
-    if (!image) res.status(400).json({ message: "image is required" });
-    if (!type) res.status(400).json({ message: "type is required" });
-    if (!degreeOfAlcohol)
-      res.status(400).json({ message: "degrees of alcohol is required" });
-    if (!description)
-      res.status(400).json({ message: "description is required" });
-    if (!qualification)
-      res.status(400).json({ message: "qualification is required" });
-    if (!price) res.status(400).json({ message: "price is required" });
-    if (!stock) res.status(400).json({ message: "stock is required" });
-    if (!presentation)
-      res.status(400).json({ message: "presentation is required" });
+    if (!name) return res.status(400).json({ message: "name is required" });
+    if (!image) return res.status(400).json({ message: "image is required" });
+    if (!type) return res.status(400).json({ message: "type is required" });
+    if (!degreeOfAlcohol) return res.status(400).json({ message: "degrees of alcohol is required" });
+    if (!description) return res.status(400).json({ message: "description is required" });
+    if (!price) return res.status(400).json({ message: "price is required" });
+    if (!stock) return res.status(400).json({ message: "stock is required" });
+    if (!presentation) return res.status(400).json({ message: "presentation is required" });
 
     const product = await Product.create({
       name,
@@ -36,7 +30,6 @@ const postProduct = async (req: Request, res: Response) => {
       type,
       degreeOfAlcohol,
       description,
-      qualification,
       price,
       stock,
       presentation,
