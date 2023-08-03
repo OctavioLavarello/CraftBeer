c6c627f056a7ed936db73859e76d18a46245dbec
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-08-03 12:33:29 -0300

    Agrego rutas en routes/indes.ts

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,13 +1,17 @@
 import {Router} from 'express'
 import postUserPerson from "../controller/postUserPerson"
 import postCompany from "../controller/postCompany";
+import getAllCompanies from '../controller/getAllCompanies';
+import postProduct from '../controller/postProduct';
 const router = Router();
 
 //-------- post routes -----//
-router.post("/user", postUserPerson)
-router.post("/company", postCompany)
+router.post("/user", postUserPerson);
+router.post("/company", postCompany);
+router.post("/product", postProduct);
 
 // ------- get routes ------- //
+router.get("/companies", getAllCompanies);
 
 
 // ------- update routes-------//
