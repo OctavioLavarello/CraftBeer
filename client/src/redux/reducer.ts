779d1d482937af04af7b1e437c621cdf4243841f
MGConil <germanconil@hotmail.com>
2023-08-03 10:38:52 -0300

    modularizar reducer

--- client/src/redux/reducer.ts
+++ client/src/redux/reducer.ts
@@ -1,36 +1,32 @@
 import { ActionWithPayload } from "./actions/actions";
+import { getAllBeer, orderFilters } from "./reducerFunctions";
 
 /* import { Action } from 'redux';
  */
-interface AppState {
+export interface AppState {
   allBeer: object[];
   beerFilters: string[];
 };
-const initialState: AppState = {
+export const initialState: AppState = {
   beerFilters: [],
   allBeer: []
 };
 
 
-const rootReducer = (state = initialState, action: ActionWithPayload <string, any>) => {
+const rootReducer = (state = initialState, action: ActionWithPayload<string, any>) => {
   switch (action.type) {
 
     case "ADD_ALL_BEER": {
-      return {
-        ...state,
-        allBeer: action.payload
-      }
+      return getAllBeer(state, action)
     }
     case "ORDER_FILTERS": {
-      return {
-        ...state,
-        beerFilters: [...state.beerFilters, action.payload]
-      }
+      return orderFilters(state, action)
     }
-
+    
     default:
       return state;
   }
 };
 
 export default rootReducer;
+
