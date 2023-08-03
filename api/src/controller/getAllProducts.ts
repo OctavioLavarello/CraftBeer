3722195cf00508a4269265ccb55c00e5d88189a8
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-08-03 19:30:36 -0300

    Rf-api-getAllProducts

--- api/src/controller/getAllProducts.ts
+++ api/src/controller/getAllProducts.ts
@@ -4,34 +4,24 @@ import { Op } from 'sequelize';
 
 export const getAllProducts = async (req: Request, res: Response) => {
   try {
-    const { price, order } = req.query;
+    const { type, order } = req.query;
 
     // Definir las opciones de consulta
     const options: any = {};
 
     // Si se proporciona un query param 'type', filtrar por tipo
-    if (price) {
-        options.where = { price: { [Op.lte]: price } };
+    if (type) {
+      options.where = { type: { [Op.eq]: type } };
     }
 
-    // // Si se proporciona un query param 'order', ordenar según el valor (asc o desc)
-    // if (order && (order === 'OrderAscPrice' || order === 'OrderDesPrice')) {
-    //   options.order = [['price', order]]; // Ordenar por precio ascendente o descendente
-    // }
+    // Ordenar según el valor de order (asc o desc)
+    if (order && (order === 'OrderAscPrice' || order === 'OrderDesPrice')) {
+      const columnToOrderBy = order === 'OrderAscPrice' ? 'price' : [['price', 'DESC']];
+      options.order = [columnToOrderBy];
+    }
 
-
-    // Si se proporciona un query param 'order', ordenar según el valor (asc o desc)
-if (order && (order === 'OrderAscPrice' || order === 'OrderDesPrice')) {
-    const columnToOrderBy = order === 'OrderAscPrice' ? 'price' : [['price', 'DESC']];
-  
-    options.order = [columnToOrderBy];
-  }
-  
-
-
-
-    // Obtener las cervezas según las opciones de consulta
-    const products = await Product.findAll(options);
+    // Obtener todos los productos si no se proporciona el query param 'type'
+    const products = await Product.findAll(type ? options : undefined);
 
     res.status(200).json(products);
   } catch (error) {
