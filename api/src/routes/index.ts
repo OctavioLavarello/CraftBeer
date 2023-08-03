e78114ed210c147b95f09f17a74daf24b1543857
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-08-03 11:33:17 -0300

    Creo controlador getCompany

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,13 +1,15 @@
 import {Router} from 'express'
 import postUserPerson from "../controller/postUserPerson"
 import postCompany from "../controller/postCompany";
+import getCompanys from '../controller/getCompanys';
 const router = Router();
 
 //-------- post routes -----//
-router.post("/user", postUserPerson)
-router.post("/company", postCompany)
+router.post("/user", postUserPerson);
+router.post("/company", postCompany);
 
 // ------- get routes ------- //
+router.get("/companys", getCompanys);
 
 
 // ------- update routes-------//
