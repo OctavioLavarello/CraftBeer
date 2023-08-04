f267093c3d64b18521165a30ce2d9e7ae3d04eb7
lixicalo <xcl_xioma@hotmail.com>
2023-08-03 23:07:57 -0500

    base-de-datos-beta

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,15 @@
 import { sequelize } from "./db"; // Ajusta la ruta si es necesario
 import server from "./src/app"; // Ajusta la ruta si es necesario
 import dotenv from "dotenv";
+import dataBase from "./helpers/baseDeDatos"
 
 dotenv.config();
 
+
+
 // Sincronizar la base de datos y levantar el servidor
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
+  dataBase()
   server.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
   });
