bca468c9c2a6f023d678d53a014a78d7633aeefa
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-08-04 10:27:08 -0300

    eliminar estos cambios

--- api/db.ts
+++ api/db.ts
@@ -4,12 +4,16 @@ import { Sequelize, Dialect } from "sequelize";
 import fs from "fs";
 import path from "path";
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST,DB_NAME, DB_DEPLOY } = process.env;
 
-const sequelize = new Sequelize(`${DB_DEPLOY}`, {
-  logging: false,
-  native: false,
-});
+// const sequelize = new Sequelize(`${DB_DEPLOY}`, {
+//   logging: false,
+//   native: false,
+// });
+
+
+
+
 
 const basename = path.basename(__filename);
 
