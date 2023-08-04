b1a0e9aa0aaa0823a1a1eaed5ce1e66df9170a5e
agustinrosa22 <agustinrosa1234@gmail.com>
2023-08-04 12:49:28 -0300

    detail funcional terminado

--- client/src/views/Detail/Detail.tsx
+++ client/src/views/Detail/Detail.tsx
@@ -1,76 +1,62 @@
-/// IMPORTS
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import styles from './Detail.module.css';
-// STYLES
-//.....
-// const beer = {
-//   id: 1,
-//   name: "Cerveza Artesanal",
-//   image: "https://images.unsplash.com/photo-1600213903598-25be92abde40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29yb25hJTIwYmVlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
-//   type: "Artesanal",
-//   degreeOfAlcohol: "5.0%",
-//   description: "Una deliciosa cerveza artesanal.",
-//   qualification: 4.5,
-//   price: 2.5,
-//   stock: 50,
-//   presentation: "Botella",
-//   companyId: 123,
-//   status: "Disponible",
-// };
-// DETAIL
+
 interface Beer {
-id: string;
+  id: string;
   name: string;
   image: string;
   type: string;
-  degreeOfAlcohol: string;
+  degreeOfAlcohol: number;
   description: string;
-  qualification: number;
+  qualification: number | null;
   price: number;
+  ABV: number,
   stock: number;
   presentation: string;
-  companyId: number;
-  status: string;
+  status: boolean;
 }
-const Detail = () => {
-  const {idProduct} = useParams();
-  const [beer, setBeer] = useState<Beer| null>(null); 
-  ({});
 
-  useEffect(()=>{
-    const fetchBeer = async () =>{
+const Detail = () => {
+  const { id } = useParams();
+  const [beer, setBeer] = useState<Beer | null>(null);
+
+  useEffect(() => {
+    const fetchBeer = async () => {
       try {
-        const response = await axios.get(`http://localhost:3001//product/${idProduct}`)
-        setBeer(response.data)
+        const response = await axios.get(`http://localhost:3001/product/${id}`);
+        setBeer(response.data);
       } catch (error) {
         console.log(error);
-        console.error('Error fetching beer', error)
+        console.error('Error fetching beer', error);
       }
     };
     fetchBeer();
-  }, [idProduct])
+  }, [id]);
+
   if (!beer) {
-    return <div>Loading...</div>; 
+    return <div>Loading...</div>;
   }
+console.log(beer);
+
   return (
     <div className={styles.container}>
       <div className={styles.datacontainer}>
         <h1>Detail</h1>
         <h2>Nombre: {beer?.name}</h2>
         <h2>Tipo: {beer?.type}</h2>
-        <h2>Graduacion alcohólica(ABV): {beer?.degreeOfAlcohol}</h2>
+        <h2>Graduacion alcohólica(ABV): {beer?.ABV}%</h2>
         <h2>Precio: ${beer?.price}</h2>
         <h2>Informacion: {beer?.description}</h2>
         <h2>Calificacion: {beer?.qualification}</h2>
         <h2>Presentacion: {beer?.presentation}</h2>
-        <h2>Estatus: {beer?.status}</h2>
+        <h2>Estatus: {beer?.status ? 'Disponible' : 'No disponible'}</h2>
+        <Link className={styles.link} to="/home">Go Back</Link>
       </div>
       <div className={styles.imagecontainer}>
         <img src={beer?.image} alt="" />
       </div>
-      <Link className={styles.link} to="/home">Go Back</Link>
     </div>
   );
 };
