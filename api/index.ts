68bf899193e958dafb2f6b9c30f1c00d41e12d19
OctavioLavarello <OctavioLavarello@gmail.com>
2023-08-04 11:52:17 -0300

    Cambios minimos al back xd

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,13 @@
 import { sequelize } from "./db"; // Ajusta la ruta si es necesario
 import server from "./src/app"; // Ajusta la ruta si es necesario
 import dotenv from "dotenv";
+const port = process.env.PORT || 3001
 
 dotenv.config();
 
 // Sincronizar la base de datos y levantar el servidor
 sequelize.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(port, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
   });
 });
