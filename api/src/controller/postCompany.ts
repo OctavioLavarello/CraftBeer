0b4d76582c22e6c753ad32035d2f96279f75a659
Alexis Weber <Alexis.Weber@mailfence.com>
2023-08-08 16:19:15 -0300

    refactor postCompany, add validation name, company,password,email,document, pgones

--- api/src/controller/postCompany.ts
+++ api/src/controller/postCompany.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { UserCompany } from "../../db";
-import UserRole from "../emuns";
+
+
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,30}$/;
 
 const postCompany = async (req: Request, res: Response) => {
   try {
@@ -18,51 +22,65 @@ const postCompany = async (req: Request, res: Response) => {
       address,
       brand,
       image,
-      role,
     } = req.body;
 
-    if (!name) res.status(400).json({ message: "name is require" });
-    if (!lastName) res.status(400).json({ message: "lastName is required" });
-    if (!document) res.status(400).json({ message: "document is required" });
-    if (!company) res.status(400).json({ message: "company is required" });
-    if (!country) res.status(400).json({ message: "country is required" });
-    if (!state) res.status(400).json({ message: "state is required" });
-    if (!city) res.status(400).json({ message: "city is required" });
-    if (!address) res.status(400).json({ message: "address is required" });
-    if (!email) res.status(400).json({ message: "email is required" });
-    if (!phone) res.status(400).json({ message: "phone number is required" });
-    if (!password) res.status(400).json({ message: "password is required" });
-    if (!Object.values(UserRole).includes(req.body.role)) {
-      return res.status(400).json({ message: "Invalid role" });
-    }
+    if (!name) return res.status(400).json({message:"Name is required."});
+    if (/\d/.test(name)) return res.status(400).json({message:"Name cannot include a number."});
+    if (name.length > 30) return res.status(400).json({message:"Name cannot exceed 30 characters."});
+    
+  
+    if (!company) return res.status(400).json({message:"Last name is required."});
+    if (/\d/.test(company)) return res.status(400).json({message:"Last name cannot include a number."});
+    if (company.length > 30) return res.status(400).json({message:"Last name cannot exceed 30 characters."});
+    
+  
+    if (!document) return res.status(400).json({message:"Document is required."});
+    if (isNaN(document)) return res.status(400).json({message:"Document must be a valid number."});
+    
+  
+    if (!phone) return res.status(400).json({message:"Phone is required."});
+    if (isNaN(phone)) return res.status(400).json({message:"Phone must be a valid number."});
+    
+  
+    if (!email) return res.status(400).json({message:"Email is required."});
+    if (!emailRegex.test(email)) return res.status(400).json({message:"Invalid email address"});
+    
+  
+    if (!password) return res.status(400).json({message:"Password is required."});
+    if (!passwordRegex.test(password)) return res.status(400).json({message:"Password must contain at least one uppercase letter, one lowercase letter, and one digit. (6 - 30 char)"});
+    
 
-    const userCompany = await UserCompany.create({
-      name,
-      lastName,
-      document,
-      company,
-      country,
-      state,
-      city,
-      address,
-      email,
-      phone,
-      password,
-      image,
-      brand,
-      status: true,
-      role,
-    });
-
-    return res.status(200).json(userCompany);
+    
+      
+      const userCompany = await UserCompany.create({
+        name,
+        lastName,
+        document,
+        company,
+        country,
+        state,
+        city,
+        address,
+        email,
+        phone,
+        password,
+        image,
+        brand,
+        status: true,
+        role: "Company",
+      });
+      
+     console.log(userCompany);
+     
+      return res.status(200).json(userCompany);
+  
   } catch (error) {
     if (error instanceof Error) {
-     return res.status(500).send(error.message);
-
+      return res.status(500).send(error.message);
     } else {
       return res.status(500).send("Unexpected error");
     }
-  }
 };
+}
 
 export default postCompany;
