e9726cdd8a87a52e44cfd7ed1e4f0c20ee53cd76
josegalvis2115 <jnrgalvis@gmail.com>
2023-08-09 11:02:30 -0500

    validaciones postUser

--- api/src/controller/postUserPerson.ts
+++ api/src/controller/postUserPerson.ts
@@ -1,21 +1,25 @@
 import { UserPerson } from "../../db";
 import { Request, Response } from "express";
-import UserRole from "../emuns";
+import postUserValidation from "../validations/postUserValidations";
 
 const postUserPerson = async (req: Request, res: Response) => {
   try {
-    const { name, lastName, document, email, password, address, image, role, country, city, state } =
+    const { name, lastName, document, email, password, address, image, country, city } =
       req.body;
 
-    if (!name) res.status(400).json({ message: "name is required" });
-    if (!lastName) res.status(400).json({ message: "lastName is require" });
-    if (!document) res.status(400).json({ message: "document is required" });
-    if (!email) res.status(400).json({ message: "email is equired" });
-    if (!password) res.status(400).json({ message: "password is required" });
-    if (!Object.values(UserRole).includes(req.body.role)) {
-      return res.status(400).json({ message: "Invalid role" });
-    }
-    
+     const errors = postUserValidation(
+      name, 
+      lastName, 
+      document, 
+      email, 
+      password, 
+      address, 
+      image, 
+      country, 
+      city
+     ) 
+
+     if (errors) return res.status(400).json({message: errors})
 
     const userPerson = await UserPerson.create({
       name,
@@ -23,21 +27,19 @@ const postUserPerson = async (req: Request, res: Response) => {
       document,
       email,
       password,
-      country,
-      city,
-      state, 
       address,
       image,
       status: true,
-      role,
+      country,
+      city,
     });
 
-    return res.status(200).json(userPerson);
+    res.status(200).json(userPerson);
   } catch (error) {
     if (error instanceof Error) {
-      return res.status(500).send(error.message);
+      res.status(500).send(error.message);
     } else {
-      return res.status(500).send("Unexpected error.");
+      res.status(500).send("Unexpected error.");
     }
   }
 };
