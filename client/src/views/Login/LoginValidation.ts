526f9b6e66737ea45d2c697f7f9160a8f0d6cca5
OctavioLavarello <OctavioLavarello@gmail.com>
2023-08-11 10:39:22 -0300

    validacion de login

--- client/src/views/Login/LoginValidation.ts
+++ client/src/views/Login/LoginValidation.ts
@@ -4,8 +4,8 @@ const loginValidation = (userLogin: login, setErrors: React.Dispatch<React.SetSt
     const { email, password } = userLogin
     // REGEX
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const uppercaseRegex = /[A-Z]/;
-    const numberRegex = /[0-9]/;
+    // const uppercaseRegex = /[A-Z]/;
+    // const numberRegex = /[0-9]/;
     // EMAIL VALIDATION
     if (!email) {
         setErrors((prevErrors) => ({
@@ -34,16 +34,16 @@ const loginValidation = (userLogin: login, setErrors: React.Dispatch<React.SetSt
             ...prevErrors,
             password: "Enter your password",
         }));
-    } else if (!uppercaseRegex.test(password)){
-        setErrors((prevErrors) => ({
-            ...prevErrors,
-            password: "Must have at least one capital letter"
-        }))
-    } else if (!numberRegex.test(password)){
-        setErrors((prevErrors) => ({
-            ...prevErrors,
-            password: "Must have at least one number"
-        }))
+    // } else if (!uppercaseRegex.test(password)){
+    //     setErrors((prevErrors) => ({
+    //         ...prevErrors,
+    //         password: "Must have at least one capital letter"
+    //     }))
+    // } else if (!numberRegex.test(password)){
+    //     setErrors((prevErrors) => ({
+    //         ...prevErrors,
+    //         password: "Must have at least one number"
+    //     }))
     } else {
         setErrors((prevErrors) => ({
             ...prevErrors,
