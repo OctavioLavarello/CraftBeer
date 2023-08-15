1a191f19a8b9259e715f55d93ab500c7a78be975
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-08-15 01:49:50 -0300

    Rf-putProduct-stock

--- api/src/controller/putProduct.ts
+++ api/src/controller/putProduct.ts
@@ -3,25 +3,29 @@ import { Product } from '../../db';
 
 const putProduct = async (req: Request, res: Response) => {
   const productId = req.params.productId;
-  const { companyId, ...updatedData } = req.body;
+  const { companyId, totalPrice, ...updatedData } = req.body;
 
   try {
     const product = await Product.findByPk(productId);
 
     if (!product) {
-      return res.status(404).send({ message: "Product not found" });
+      return res.status(404).send({ message: "Product not found." });
+    }
+
+    if(!companyId && !totalPrice){
+      return res.status(404).send({ message: "Required information." });
     }
     
-    if (companyId !== product.companyId) {
-      return res.status(403).send({ message: "You do not have permission to modify this product" });
+    if (companyId && companyId !== product.companyId) {
+      return res.status(403).send({ message: "You do not have permission to modify this product." });
     }
 
     await product.update(updatedData, { fields: Object.keys(updatedData) });
 
-    return res.status(200).send({ message: "Product updated successfully" });
+    return res.status(200).send({ message: "Product updated successfully." });
   } catch (error) {
     console.error("Error updating the product:", error);
-    return res.status(500).send({ message: "Internal server error" });
+    return res.status(500).send({ message: "Internal server error." });
   }
 };
 
