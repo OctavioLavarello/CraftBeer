100193800ec45ea405722304a2270d0471fcfa62
OctavioLavarello <OctavioLavarello@gmail.com>
2023-08-16 23:24:53 -0300

    avances del dash

--- client/src/views/Company/Company.tsx
+++ client/src/views/Company/Company.tsx
@@ -47,7 +47,11 @@ const Company: React.FC = () => {
         company: "",
         address: ""
     });
+    const [isClicked, setIsClicked] = useState<boolean>(false);
     // HANDLERS
+    const handlerIsClicked = () => {
+        setIsClicked(!isClicked)
+    }
     const handlerOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
         setCompanyData((prevCompanyData) => ({
@@ -58,120 +62,247 @@ const Company: React.FC = () => {
     };
     const handlerOnSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
         event.preventDefault();
+        // Dispatch de PUT company
+        setIsClicked(!isClicked)
     };
+    console.log(isClicked)
     return (
         <div className={styles.all}>
-            <Form 
-            className={styles.form}
-            onSubmit={handlerOnSubmit}
-            >
-                <Form.Group>
-                    <Form.Control 
-                    required
-                    type="text"
-                    name="name"
-                    value={companyData.name}
-                    placeholder="change your name?"
-                    onChange={handlerOnChange}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control 
-                    required
-                    type="text"
-                    name="lastName"
-                    value={companyData.lastName}
-                    placeholder="change your last name?"
-                    onChange={handlerOnChange}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control 
-                    required
-                    type="email" 
-                    name="email"
-                    value={companyData.email}
-                    placeholder="change your email?"
-                    onChange={handlerOnChange} 
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control 
-                    required
-                    type="number"
-                    name="document"
-                    value={companyData.document}
-                    placeholder="change your document?"
-                    onChange={handlerOnChange}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control 
-                    required
-                    type="number"
-                    name="phone"
-                    value={companyData.phone}
-                    placeholder="change your phone number?"
-                    onChange={handlerOnChange}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control 
-                    required
-                    type="text"
-                    name="city"
-                    value={companyData.country}
-                    placeholder="Password"
-                    onChange={handlerOnChange}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control 
-                    required
-                    type="text"
-                    name="password"
-                    value={companyData.state}
-                    placeholder="Password"
-                    onChange={handlerOnChange}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control 
-                    required
-                    type="text"
-                    name="password"
-                    value={companyData.city}
-                    placeholder="Password"
-                    onChange={handlerOnChange}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control 
-                    required
-                    type="text"
-                    name="password"
-                    value={companyData.company}
-                    placeholder="Password"
-                    onChange={handlerOnChange}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control 
-                    required
-                    type="text"
-                    name="password"
-                    value={companyData.address}
-                    placeholder="Password"
-                    onChange={handlerOnChange}
-                    />
-                </Form.Group>
-                <button 
-                type="submit"
-                className={styles.submit}
+            <div className={styles.formMaster}>
+                {!isClicked ? 
+                (
+                <div className={styles.form}>
+                    <Form 
+                    className={styles.form}
+                    >
+                        <Form.Group>
+                            <Form.Control 
+                            readOnly
+                            type="text"
+                            name="name"
+                            value={companyData.name}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control 
+                            readOnly
+                            type="text"
+                            name="lastName"
+                            value={companyData.lastName}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control 
+                            readOnly
+                            type="email" 
+                            name="email"
+                            value={companyData.email}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control 
+                            readOnly
+                            type="number"
+                            name="document"
+                            value={companyData.document}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control 
+                            readOnly
+                            type="number"
+                            name="phone"
+                            value={companyData.phone}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control 
+                            readOnly
+                            type="text"
+                            name="city"
+                            value={companyData.country}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control 
+                            readOnly
+                            type="text"
+                            name="password"
+                            value={companyData.state}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control 
+                            readOnly
+                            type="text"
+                            name="password"
+                            value={companyData.city}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control 
+                            readOnly
+                            type="text"
+                            name="password"
+                            value={companyData.company}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control 
+                            readOnly
+                            type="text"
+                            name="password"
+                            value={companyData.address}
+                            />
+                        </Form.Group>
+                    </Form>
+                    <button 
+                    onClick={handlerIsClicked}
+                    className={styles.edit}
+                    >
+                        Edit Profile
+                    </button>
+                </div>
+                ) : 
+                (
+                <Form 
+                className={styles.form}
+                onSubmit={handlerOnSubmit}
                 >
-                    Save changes
-                </button>
-            </Form>
+                    <Form.Group>
+                        <Form.Control 
+                        required
+                        type="text"
+                        name="name"
+                        value={companyData.name}
+                        placeholder="change your name?"
+                        onChange={handlerOnChange}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control 
+                        required
+                        type="text"
+                        name="lastName"
+                        value={companyData.lastName}
+                        placeholder="change your last name?"
+                        onChange={handlerOnChange}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control 
+                        required
+                        type="email" 
+                        name="email"
+                        value={companyData.email}
+                        placeholder="change your email?"
+                        onChange={handlerOnChange} 
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control 
+                        required
+                        type="number"
+                        name="document"
+                        value={companyData.document}
+                        placeholder="change your document?"
+                        onChange={handlerOnChange}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control 
+                        required
+                        type="number"
+                        name="phone"
+                        value={companyData.phone}
+                        placeholder="change your phone number?"
+                        onChange={handlerOnChange}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control 
+                        required
+                        type="text"
+                        name="city"
+                        value={companyData.country}
+                        placeholder="Password"
+                        onChange={handlerOnChange}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control 
+                        required
+                        type="text"
+                        name="password"
+                        value={companyData.state}
+                        placeholder="Password"
+                        onChange={handlerOnChange}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control 
+                        required
+                        type="text"
+                        name="password"
+                        value={companyData.city}
+                        placeholder="Password"
+                        onChange={handlerOnChange}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control 
+                        required
+                        type="text"
+                        name="password"
+                        value={companyData.company}
+                        placeholder="Password"
+                        onChange={handlerOnChange}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control 
+                        required
+                        type="text"
+                        name="password"
+                        value={companyData.address}
+                        placeholder="Password"
+                        onChange={handlerOnChange}
+                        />
+                    </Form.Group>
+                    <button 
+                    type="submit"
+                    className={styles.submit}
+                    disabled={
+                        !companyData.name ||
+                        !companyData.lastName ||
+                        !companyData.email ||
+                        !companyData.document ||
+                        !companyData.phone ||
+                        !companyData.country ||
+                        !companyData.city ||
+                        !companyData.state ||
+                        !companyData.company ||
+                        !companyData.address ||
+                        !!errors.name ||
+                        !!errors.lastName ||
+                        !!errors.email ||
+                        !!errors.document ||
+                        !!errors.phone ||
+                        !!errors.country ||
+                        !!errors.city ||
+                        !!errors.state ||
+                        !!errors.company ||
+                        !!errors.address
+                        }      
+                    >
+                        Save changes
+                    </button>
+                </Form>
+                )}
+            </div>
+            <div className={styles.sold}>
+                <h2>HISTORIAL DE CERVEZAS VENDIDAS</h2>
+            </div>
         </div>
     );
 }
