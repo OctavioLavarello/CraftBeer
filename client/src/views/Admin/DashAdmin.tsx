e3d7dcc6fe69e4df4ec9ee79e212b770b6966053
ESGuaimacuto <samguaima@gmail.com>
2023-08-18 00:06:53 -0400

    Actualización Panel Admin

--- client/src/views/Admin/DashAdmin.tsx
+++ client/src/views/Admin/DashAdmin.tsx
@@ -5,47 +5,46 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import DetailBuyer from "../Admin/DetailBuyer"
-
+import { idBuyer, idSeller } from "../../redux/actions/actions";
 
 const Administrador = () => {
-const dispatch = useDispatch<any>()
+  const dispatch = useDispatch<any>()
   const [sellers, setSellers] = useState<any>([]);
   const [buyers, setBuyers] = useState<any>([]);
   const [type, setType] = useState("");
 
-    interface User {
-        address: string,
-        city: string,
-        country: string
-        document: number,
-        email: string,
-        id: string,
-        image: string,
-        lastName: string,
-        name: string
-        password: string
-        role: string,
-        state: string
-        status: boolean
-    }
+  interface User {
+    address: string;
+    city: string;
+    country: string;
+    document: number;
+    email: string;
+    id: string;
+    image: string;
+    lastName: string;
+    name: string;
+    password: string;
+    role: string;
+    state: string;
+    status: boolean;
+  }
 
-    interface Comapny {
-        address: string,
-        company: string,
-        city: string,
-        country: string
-        document: number,
-        email: string,
-        id: string,
-        image: string,
-        lastName: string,
-        name: string
-        password: string
-        role: string,
-        state: string
-        status: boolean
-    }
+  interface Comapny {
+    address: string;
+    company: string;
+    city: string;
+    country: string;
+    document: number;
+    email: string;
+    id: string;
+    image: string;
+    lastName: string;
+    name: string;
+    password: string;
+    role: string;
+    state: string;
+    status: boolean;
+  }
 
   const handlerSeller = async () => {
     try {
@@ -53,8 +52,8 @@ const dispatch = useDispatch<any>()
       setSellers(response.data);
       setType("seller");
       console.log(sellers);
-      //console.log(type);
-      toast.success("Data cargada satisfactoriamente");
+        console.log(type);
+      toast.success("Cargada la información de los vendedores");
     } catch (error) {
       toast.error("No ha sido posible cargar la data");
     }
@@ -67,22 +66,21 @@ const dispatch = useDispatch<any>()
       setType("buyer");
       console.log(buyers);
       console.log(type);
-      toast.success("Data cargada satisfactoriamente");
+      toast.success("Cargada información de los compradores");
     } catch (error) {
       toast.error("No ha sido posible cargar la data");
     }
   };
 
-  const handlerLink = (event)=> {
-    if(type === "buyer"){
-        const id:string = event.target.name
-        console.log(id);
-        DetailBuyer(id)
+  const handlerLink = async (event: any) => {
+    if (type === "buyer") {
+      const id: string | any = event.target.name;
+      await dispatch(idBuyer(id))
     } else {
-        toast.error("No es posible acceder a la información")
+      const codigo = event.target.name
+      await dispatch(idSeller(codigo))
     }
-  }
-  
+  };
 
   return (
     <div style={{ margin: "0.5%" }}>
@@ -98,75 +96,81 @@ const dispatch = useDispatch<any>()
           </Button>
         </Col>
       </Row>
-    {type === "buyer" ? 
-(      <Table striped bordered hover variant="" responsive>
-        <thead className="bordeTabla">
-          <tr className="bordeTabla">
-            <th className="anchoTable">Detalle</th>
-            <th className="anchoTable">Correo electrónico</th>
-            <th className="anchoTable">Nombre</th>
-            <th className="anchoTable">Apellidos</th>
-            <th className="anchoTable">Doc Identidad</th>
-            <th className="anchoTable">País</th>
-            <th className="anchoTable">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {buyers?.map((person:User) => {
-            return (
-              <tr>
-                <td><Link  onClick={handlerLink} to={`/admin/buyer/${person.id}`}>
-                    <Button name={person.id}>
-                    Detalle User   
-                    </Button>
-                    </Link></td>
-                <td>{person.email}</td>
-                <td>{person.name}</td>
-                <td>{person.lastName}</td>
-                <td>{person.document}</td>
-                <td>{person.country}</td>
-                <td>{person.status === true ? "Activo" : "Inactivo"}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>)
-       :
-(       <Table>
-        <thead className="bordeTabla">
-       <tr className="bordeTabla">
-         <th className="anchoTable">ID</th>
-         <th className="anchoTable">Nombre compañía</th>
-         <th className="anchoTable">Correo electrónico</th>
-         <th className="anchoTable">Nombre</th>
-         <th className="anchoTable">Apellidos</th>
-         <th className="anchoTable">Doc Identidad</th>
-         <th className="anchoTable">País</th>
-         <th className="anchoTable">Status</th>
-       </tr>
-     </thead>
-     <tbody>
-       {sellers?.map((cia:Comapny) => {
-         return (
-           <tr>
-               <td><Link to={`/admin/seller/${cia.id}`}>
-                    <Button name={cia.id} onClick={handlerLink}>
-                    Detalle User   
-                    </Button>
-                    </Link></td>
-             <td><strong>{cia.company}</strong></td>
-             <td>{cia.email}</td>
-             <td>{cia.name}</td>
-             <td>{cia.lastName}</td>
-             <td>{cia.document}</td>
-             <td>{cia.country}</td>
-             <td>{cia.status === true ? "Activo" : "Inactivo"}</td>
-           </tr>
-         );
-       })}
-     </tbody>
-       </Table>) 
-       }
+      {type === "buyer" ? (
+        <Table striped bordered hover variant="" responsive>
+          <thead className="bordeTabla">
+            <tr className="bordeTabla">
+              <th className="anchoTable">Detalle</th>
+              <th className="anchoTable">Correo electrónico</th>
+              <th className="anchoTable">Nombre</th>
+              <th className="anchoTable">Apellidos</th>
+              <th className="anchoTable">Doc Identidad</th>
+              <th className="anchoTable">País</th>
+              <th className="anchoTable">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {buyers?.map((person: User) => {
+              return (
+                <tr>
+                  <td>
+                    <Link to={`/admin/buyer/${person.id}`}>
+                      <Button name={person.id} onClick={handlerLink}>
+                        Detalle User
+                      </Button>
+                    </Link>
+                  </td>
+                  <td>{person.email}</td>
+                  <td>{person.name}</td>
+                  <td>{person.lastName}</td>
+                  <td>{person.document}</td>
+                  <td>{person.country}</td>
+                  <td>{person.status === true ? "Activo" : "Inactivo"}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      ) : (
+        <Table>
+          <thead className="bordeTabla">
+            <tr className="bordeTabla">
+              <th className="anchoTable">ID</th>
+              <th className="anchoTable">Nombre compañía</th>
+              <th className="anchoTable">Correo electrónico</th>
+              <th className="anchoTable">Nombre</th>
+              <th className="anchoTable">Apellidos</th>
+              <th className="anchoTable">Doc Identidad</th>
+              <th className="anchoTable">País</th>
+              <th className="anchoTable">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sellers?.map((cia: Comapny) => {
+              return (
+                <tr>
+                  <td>
+                    <Link to={`/admin/seller/${cia.id}`}>
+                      <Button name={cia.id} onClick={handlerLink}>
+                        Detalle User
+                      </Button>
+                    </Link>
+                  </td>
+                  <td>
+                    <strong>{cia.company}</strong>
+                  </td>
+                  <td>{cia.email}</td>
+                  <td>{cia.name}</td>
+                  <td>{cia.lastName}</td>
+                  <td>{cia.document}</td>
+                  <td>{cia.country}</td>
+                  <td>{cia.status === true ? "Activo" : "Inactivo"}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      )}
     </div>
   );
 };
