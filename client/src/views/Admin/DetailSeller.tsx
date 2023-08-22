9d533b8c32e2bee82b7929dc380b29ea4ffb89be
ESGuaimacuto <samguaima@gmail.com>
2023-08-22 14:58:47 -0400

    Ajustaes al Admin

--- client/src/views/Admin/DetailSeller.tsx
+++ client/src/views/Admin/DetailSeller.tsx
@@ -80,7 +80,7 @@ const DetailSeller = () => {
           <NavDropdown title="Otras opciones" menuVariant="dark">
             <NavDropdown.Item>Historial de ventas</NavDropdown.Item>
             <NavDropdown.Item>
-              <Link to="/admin/seller/products">Productos en venta</Link>
+              <Link to="/admin/seller/products" style={{textDecoration:"none", color:"white"}}>Productos en venta</Link>
             </NavDropdown.Item>
             <NavDropdown.Item>Modificar usuario</NavDropdown.Item>
           </NavDropdown>
@@ -88,13 +88,13 @@ const DetailSeller = () => {
         <Row style={{ height: "100%", width: "100%", margin: "0.5%" }}>
           <Col style={{ width: "50%", height: "auto" }}>
             Nombre
-            <input type="text" className="inputBuyer" value={infoUser.name} />
+            <input type="text" className="inputSeller" value={infoUser.name} />
           </Col>
           <Col>
             Apellido
             <input
               type="text"
-              className="inputBuyer"
+              className="inputSeller"
               value={infoUser.lastName}
             />
           </Col>
@@ -102,7 +102,7 @@ const DetailSeller = () => {
             Dirección de la compañía
             <input
               type="text"
-              className="inputBuyer"
+              className="inputSeller"
               value={infoUser.address}
             />
           </Col>
@@ -110,19 +110,19 @@ const DetailSeller = () => {
         <Row style={{ height: "100%", width: "100%", margin: "0.5%" }}>
           <Col style={{ width: "50%", height: "auto" }}>
             Estado/Región/Provincia
-            <input type="text" className="inputBuyer" value={infoUser.state} />
+            <input type="text" className="inputSeller" value={infoUser.state} />
           </Col>
           <Col>
             País
             <input
               type="text"
-              className="inputBuyer"
+              className="inputSeller"
               value={infoUser.country}
             />
           </Col>
           <Col>
             Teléfono móvil
-            <input type="text" className="inputBuyer" value={infoUser.phone} />
+            <input type="text" className="inputSeller" value={infoUser.phone} />
           </Col>
         </Row>
         <Row style={{ height: "100%", width: "100%", margin: "0.5%" }}>
@@ -130,34 +130,35 @@ const DetailSeller = () => {
             Documento de identidad
             <input
               type="text"
-              className="inputBuyer"
+              className="inputSeller"
               value={infoUser.document}
             />
           </Col>
           <Col>
             Tipo de usuario
-            <input type="text" className="inputBuyer" value={infoUser.role} />
+            <input type="text" className="inputSeller" value={infoUser.role} />
           </Col>
           <Col></Col>
         </Row>
         <Row style={{ height: "100%", width: "100%", margin: "0.5%" }}>
           <Col style={{ width: "50%", height: "100%" }}>
             Ciudad
-            <input type="text" className="inputBuyer" value={infoUser.city} />
+            <input type="text" className="inputSeller" value={infoUser.city} />
           </Col>
           <Col>
             Direccion de correo
-            <input type="text" className="inputBuyer" value={infoUser.email} />
+            <input type="text" className="inputSeller" value={infoUser.email} />
           </Col>
           <Col></Col>
         </Row>
         <div className="botonesBuyer">
-          <Link to="/admin" style={{ width: "30%" }}>
+          <Link to="/admin" style={{ width: "30%", height:"70%" }}>
             <Button
               style={{
                 width: "100%",
                 backgroundColor: "#A37D34",
                 border: "none",
+                height:"100%"
               }}
             >
               Volver al Panel
@@ -165,15 +166,15 @@ const DetailSeller = () => {
           </Link>
           <Button
             onClick={handlerActive}
-            style={{ width: "15%", backgroundColor: "black", border: "none" }}
+            style={{ width: "20%", backgroundColor: "black", border: "none", height:"70%" }}
           >
             Activar Usuario
           </Button>
           <Button
             onClick={handlerInactive}
             style={{
-              width: "15%",
-              height: "auto",
+              width: "20%",
+              height: "70%",
               backgroundColor: "black",
               border: "none",
             }}
