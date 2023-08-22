5cd2947f110bde8f2c59121bb9ae6b1cc8c8821e
Alexis Weber <Alexis.Weber@Mailfence.com>
2023-08-22 11:41:06 -0300

    FT-API/Client-Login

--- api/src/controller/logIn.ts
+++ api/src/controller/logIn.ts
@@ -3,36 +3,47 @@ import { UserCompany, UserPerson } from "../../db";
 import { error } from "console";
 
 const logIn = async (req: Request, res: Response) => {
-    try {
-      const { password, email } = req.query
-  
-      if (!email) return res.status(400).json({ message: "email is required" });
-      else if (!password) return res.status(400).json({ message: "Password is required" });
-  
-      const findUser = await UserPerson.findOne({ where: { email } });
-      const findCompany = await UserCompany.findOne({ where: { email } });
-  
-      if (!findUser && !findCompany) {
-        return res.status(404).json({ message: "user not found" });
-      }
-  
-      if (findUser) {
+  try {
+    const { password, email, email_verified } = req.query;
+    const verified = email_verified?.toString()
+
+    if (!email) return res.status(400).json({ message: "email is required" });
+
+    if (email_verified) {
+      null;
+    } else if (!password)
+      return res.status(400).json({ message: "Password is required" });
+
+    const findUser = await UserPerson.findOne({ where: { email } });
+    const findCompany = await UserCompany.findOne({ where: { email } });
+
+    if (!findUser && !findCompany) {
+      return res.status(404).json({ message: "user not found" });
+    }
+
+    if (findUser) {
+      if (verified) return res.status(200).json({ access: true, user: findUser });
+      else {
         if (findUser.password === password) {
           return res.status(200).json({ access: true, user: findUser });
         } else {
           return res.status(400).json({ message: "invalid password" });
         }
-      } else if (findCompany) {
-        if (findCompany.password === password) {
-            return res.status(200).json({ access: true, user: findCompany });
-        } else {
-            return res.status(400).json({ message: "invalid password" });
-        }
       }
-    } catch (error) {
-      return res.status(500).json({ message: "Internal server error" });
+    } else if (findCompany) {
+
+      if (verified === "true") return res.status(200).json({access:true, findCompany})
+      else {
+      if (findCompany.password === password) {
+        return res.status(200).json({ access: true, user: findCompany });
+      } else {
+        return res.status(400).json({ message: "invalid password" });
+      }
     }
-  };
-  
+  }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export default logIn;
--- client/src/views/Login/Login.tsx
+++ client/src/views/Login/Login.tsx
@@ -9,150 +9,157 @@ import { toast } from "react-hot-toast";
 import { login } from "../../redux/actions/actions";
 // STYLES
 import styles from "./Login.module.css";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import avatar from "../../assets/icons/avatar.png"
-import email from "../../assets/icons/sobre.png"
-import password from "../../assets/icons/candado.png"
-// GOOGLE LOGIN
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GoogleLogin } from '@react-oauth/google';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import avatar from "../../assets/icons/avatar.png";
+import email from "../../assets/icons/sobre.png";
+import password from "../../assets/icons/candado.png";
+
+import LoginGoogle from "../../components/loginConfig/loginConfig";
 
 export interface login {
-    email: string,
-    password: string
+  email: string;
+  password: string;
 }
 // LOGIN
 const Login: React.FC = () => {
-    // GLOBAL STATE
-    const dispatch = useDispatch<Dispatch<AnyAction> | any>();
-    // LOCAL STATES
-    const [userLogin, setUserLogin] = useState<login>({
-        email: "",
-        password: "",
-    });
-    const [errors, setErrors] = useState<login>({
-        email: "",
-        password: "",
-    });
-    const [isClicked, setIsClicked] = useState<boolean>(false);
-    const [isError, setIsError] = useState<boolean>(false);
-    const [showPassword, setShowPassword] = useState<boolean>(false);
-    // HANDLERS
-    const handlerOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
-        setUserLogin((prevUserLogin) => ({
-            ...prevUserLogin,
-            [name]: value
-        }));
-        loginValidation(userLogin, setErrors);
-    };
-    const handlerOnSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
-        event.preventDefault();
-        try {
-            await setIsClicked(!isClicked);
-            await dispatch(login(userLogin));
-        } catch (error: any) { 
-            if (error.response && error.response.data && error.response.data.message) {
-                const errorMessage = error.response.data.message;
-                toast.error(errorMessage);
-                setIsError(!isError)
-            } else {
-            toast.error("An error occurred while logging in.");
-            }
+  // ---------------------- GLOBAL STATE ------------------------//
+
+  const dispatch = useDispatch<Dispatch<AnyAction> | any>();
+
+  //  --------------------- LOCAL STATES ------------------------//
+
+  const [userLogin, setUserLogin] = useState<login>({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState<login>({
+    email: "",
+    password: "",
+  });
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  // --------------------- HANDLERS ---------------//
+
+  const handlerOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUserLogin((prevUserLogin) => ({
+      ...prevUserLogin,
+      [name]: value,
+    }));
+    loginValidation(userLogin, setErrors);
+  };
+  const handlerOnSubmit: React.FormEventHandler<HTMLFormElement> = async (
+    event
+  ) => {
+    event.preventDefault();
+    try {
+      await setIsClicked(!isClicked);
+      await dispatch(login(userLogin));
+    } catch (error: any) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        const errorMessage = error.response.data.message;
+        toast.error(errorMessage);
+        setIsError(!isError);
+      } else {
+        toast.error("An error occurred while logging in.");
+      }
+    }
+  };
+  const handlerCheck = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className={styles.all}>
+      <div
+        className={
+          !isError
+            ? !isClicked
+              ? styles.avatarCont
+              : styles.avatarContSubmit
+            : styles.avatarContError
+        }>
+        <img src={avatar} alt="avatar" className={styles.avatar} />
+      </div>
+      <Form
+        className={
+          !isError
+            ? !isClicked
+              ? styles.form
+              : styles.formSubmit
+            : styles.formError
         }
-    };
-    const handlerCheck= () => {
-        setShowPassword(!showPassword);
-    };
-    return (
-        <div className={styles.all}>
-            <div className={!isError ? (!isClicked ? styles.avatarCont : styles.avatarContSubmit) : styles.avatarContError}>
-                <img 
-                src={avatar} 
-                alt="avatar"
-                className={styles.avatar}
-                />
-            </div>
-            <Form 
-            className={!isError ? (!isClicked ? styles.form : styles.formSubmit) : styles.formError}
-            onSubmit={handlerOnSubmit}
-            >
-                <Form.Group className={`${errors.email ? styles.inputError1 : styles.input1}`}>
-                    <div>
-                        <img src={email} alt="email" />
-                    </div>
-                    <Form.Control 
-                    required
-                    type="email" 
-                    name="email"
-                    placeholder="Enter email"
-                    onChange={handlerOnChange} 
-                    />
-                </Form.Group>
-                {errors.email && <p className={styles.validationMessage}>{errors.email}</p>}
-                <Form.Group className={`${errors.password ? styles.inputError : styles.input}`}>
-                    <div>
-                        <img src={password} alt="password" />    
-                    </div>
-                    <Form.Control 
-                    required
-                    type={showPassword ? 'text' : 'password'}
-                    name="password"
-                    placeholder="Password"
-                    onChange={handlerOnChange}
-                    />
-                </Form.Group>
-                {errors.password && <p className={styles.validationMessage}>{errors.password}</p>}
-                <label className={styles.label}>
-                    We'll never share your password with anyone else.
-                </label>
-                <Form.Group className={styles.checks}>
-                    <Form.Check 
-                    type="checkbox" 
-                    label="Check me out" 
-                    onChange={handlerCheck}
-                    />
-                    <NavLink 
-                    to="/home"
-                    className={styles.forgot}
-                    >
-                        Forgot Password?
-                    </NavLink>
-                </Form.Group>
-                <button 
-                type="submit"
-                className={styles.submit}
-                disabled={
-                    !userLogin.email ||
-                    !userLogin.password ||
-                    !!errors.email ||
-                    !!errors.password
-                    }
-                >
-                    Submit
-                </button>
-                <NavLink 
-                to="/chooseSignUp">
-                    <Button 
-                    className={styles.signUp}
-                    >
-                        Sign Up
-                    </Button>
-                </NavLink>
-                <GoogleOAuthProvider clientId="613986479282-t9j4clt86pdii5h3m22abarctpm7k3d7.apps.googleusercontent.com">
-                    <GoogleLogin
-                        onSuccess={(credentialResponse: any) => {
-                            console.log(credentialResponse);
-                        }}
-                        onError={() => {
-                            console.log('Login Failed');
-                        }}
-                    />
-                </GoogleOAuthProvider>
-            </Form>
-        </div>
-    );
+        onSubmit={handlerOnSubmit}>
+        <Form.Group
+          className={`${errors.email ? styles.inputError1 : styles.input1}`}>
+          <div>
+            <img src={email} alt="email" />
+          </div>
+          <Form.Control
+            required
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            onChange={handlerOnChange}
+          />
+        </Form.Group>
+        {errors.email && (
+          <p className={styles.validationMessage}>{errors.email}</p>
+        )}
+        <Form.Group
+          className={`${errors.password ? styles.inputError : styles.input}`}>
+          <div>
+            <img src={password} alt="password" />
+          </div>
+          <Form.Control
+            required
+            type={showPassword ? "text" : "password"}
+            name="password"
+            placeholder="Password"
+            onChange={handlerOnChange}
+          />
+        </Form.Group>
+        {errors.password && (
+          <p className={styles.validationMessage}>{errors.password}</p>
+        )}
+        <label className={styles.label}>
+          We'll never share your password with anyone else.
+        </label>
+        <Form.Group className={styles.checks}>
+          <Form.Check
+            type="checkbox"
+            label="Check me out"
+            onChange={handlerCheck}
+          />
+          <NavLink to="/home" className={styles.forgot}>
+            Forgot Password?
+          </NavLink>
+        </Form.Group>
+        <button
+          type="submit"
+          className={styles.submit}
+          disabled={
+            !userLogin.email ||
+            !userLogin.password ||
+            !!errors.email ||
+            !!errors.password
+          }>
+          Submit
+        </button>
+        <NavLink to="/chooseSignUp">
+          <Button className={styles.signUp}>Sign Up</Button>
+        </NavLink>
+      </Form>
+        <LoginGoogle/>
+    </div>
+  );
 };
 
 export default Login;
