c5d29e3ab37865ee8c1e7366e1b2faeac51db534
ESGuaimacuto <samguaima@gmail.com>
2023-08-23 11:50:36 -0400

    Arreglo login

--- client/src/views/Login/Login.tsx
+++ client/src/views/Login/Login.tsx
@@ -14,7 +14,8 @@ import Form from "react-bootstrap/Form";
 import avatar from "../../assets/icons/avatar.png";
 import email from "../../assets/icons/sobre.png";
 import password from "../../assets/icons/candado.png";
-import { Button } from "react-bootstrap";
+import LoginGoogle from "../../components/loginConfig/loginConfig";
+//import { Button } from "react-bootstrap";
 
 // LOGIN
 export interface login {
