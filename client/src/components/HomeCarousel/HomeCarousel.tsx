31237297e6cf8b9e56e1ee869a8a372a45207bff
MGConil <germanconil@hotmail.com>
2023-08-23 22:50:16 -0300

    cmabio de imagenes en el home

--- client/src/components/HomeCarousel/HomeCarousel.tsx
+++ client/src/components/HomeCarousel/HomeCarousel.tsx
@@ -5,57 +5,73 @@ import { NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Carousel, CarouselItem } from "react-bootstrap";
 import styles from "./HomeCarousel.module.css";
+import image1 from "../../assets/img/image1.jpg"
+import image2 from "../../assets/img/image2.jpg"
+import image4 from "../../assets/img/image4.jpg"
+import image3 from "../../assets/img/image3.png"
 
 const HomeCarousel: React.FC = () => {
   // LOCAL STATES
   const images = [
-    { src: "https://3cordilleras.com/wp-content/uploads/2021/12/1.-nuestras-cervezas-scaled.jpeg",
-    to: 1},
-    { src:"https://lescuentoque.com.co/wp-content/uploads/2020/02/Cerveza-BBC-Cajic%C3%A1.jpg",
-    to: 1},
-    { src:"https://onlinelicor.es/wp-content/uploads/2019/02/Carlsberg-Pilsner.jpg",
-    to: 1},
-    { src:"https://www.italiaatavola.net/images/contenutiarticoli/quattro-luppoli-originale.jpg",
-    to: 1},
+    {
+      src: image4,
+      to: 1
+    },
+    {
+      src: image1,
+      to: 1
+    },
+    {
+      src: image2,
+      to: 1
+    },
+    {
+      src: image3,
+      to: 1
+    },
   ];
   return (
-    <div className={styles.div}>
-      <Carousel
-      fade={true}  
-      indicators={true} 
-      className={styles.carousel}>
-        {images.map((image, index) => (
-          <CarouselItem key={index} >
-            <div className={styles.carouselContainer}>
-              <NavLink 
-              to={`/home`}
-              className={styles.linkLeft}>
-                <img
-                  src={images[(index - 1 + images.length) % images.length].src}
-                  alt={`img${index - 1}`}
-                />
-              </NavLink>
-              <NavLink 
-              to={`/home`}
-              className={styles.linkMain}>
-                <img
-                  src={image.src}
-                  alt={`img${index}`}
-                />
-              </NavLink>
-              <NavLink 
-              to={`/home`}
-              className={styles.linkRight}>
-                <img
-                  src={images[(index + 1) % images.length].src}
-                  alt={`img${index + 1}`}
-                />
-              </NavLink>
-            </div>
-          </CarouselItem>
-        ))}
-      </Carousel> 
+    <div style={{ textAlign: "center" }}>
+      <h2 className={styles.title}>Nuestras empresas</h2>
+      <div className={styles.div}>
+        <Carousel
+          fade={true}
+          indicators={true}
+          className={styles.carousel}>
+          {images.map((image, index) => (
+            <CarouselItem key={index} >
+              <div className={styles.carouselContainer}>
+                <NavLink
+                  to={`/home`}
+                  className={styles.linkLeft}>
+                  <img
+                    src={images[(index - 1 + images.length) % images.length].src}
+                    alt={`img${index - 1}`}
+                  />
+                </NavLink>
+                <NavLink
+                  to={`/home`}
+                  className={styles.linkMain}>
+                  <img
+                    src={image.src}
+                    alt={`img${index}`}
+                  />
+                </NavLink>
+                <NavLink
+                  to={`/home`}
+                  className={styles.linkRight}>
+                  <img
+                    src={images[(index + 1) % images.length].src}
+                    alt={`img${index + 1}`}
+                  />
+                </NavLink>
+              </div>
+            </CarouselItem>
+          ))}
+        </Carousel>
+      </div>
     </div>
+
   );
 };
 
