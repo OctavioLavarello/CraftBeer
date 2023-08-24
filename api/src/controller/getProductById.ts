38cf4a023b7969245a2866b74b0e26bc76b39008
lixicalo <xcl_xioma@hotmail.com>
2023-08-23 20:34:55 -0500

    rt-api-controller-getProductById/se añadio nombre

--- api/src/controller/getProductById.ts
+++ api/src/controller/getProductById.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Product, Qualification, UserPerson } from "../../db";
+import { UUID } from "crypto";
 
 const getProductById = async (req: Request, res: Response) => {
   try {
@@ -19,9 +20,14 @@ const getProductById = async (req: Request, res: Response) => {
     if (!beer) {
       return res.status(404).send("This beer does not exist");
     }
-    const productQualification = Promise.all()
+   const productQualification = await  Promise.all(beer.Qualifications.map(async (prod:{userPersonId:UUID, get:Function})=>{
+    let person = await UserPerson.findByPk(prod.userPersonId)
+    return {
+      ...prod.get({ plain: true }), person:person.name+" "+ person.lastName, 
+    }
+   }))
 
-    return res.status(200).json(beer.get({ plain: true })); //guardar los datos planos
+    return res.status(200).json({...beer.get({ plain: true }),Qualifications:productQualification}); //guardar los datos planos
   } catch (error) {
     console.log(error);
     return res.status(500).send({ error });
